10d4412a1429d209340227217d945eb969723f5d
kwoncha <rnjsckgus@gmail.com>
2023-11-13 15:41:11 +0900

    refactor: 주문 수량과 메뉴를 알 수 있게 #orderedMenu 추가와 로직 변경

--- src/utils/Calculation/MenuCalculation.js
+++ src/utils/Calculation/MenuCalculation.js
@@ -1,33 +1,68 @@
-import { NUMBER, MENU } from '../../constants/constants.js';
+import { NUMBER, MENU, CALENDAR } from '../../constants/constants.js';
 
 class MenuCalculation {
   #menu = new Map();
+  #orderedMenu = {
+    appetizer: {
+      양송이수프: 0,
+      타파스: 0,
+      시저샐러드: 0,
+    },
+    main: {
+      티본스테이크: 0,
+      바비큐립: 0,
+      해산물파스타: 0,
+      크리스마스파스타: 0,
+    },
+    dessert: {
+      초코케이크: 0,
+      아이스크림: 0,
+    },
+    drink: {
+      제로콜라: 0,
+      레드와인: 0,
+      샴페인: 0,
+    },
+  }
 
   getdDvideMenuOrders(inputMenus) {
     return this.#menu(inputMenus.split(','));
   }
 
   getProcessIndividualOrder(inputMenus) {
-    const dividedMenuOrders = this.divideMenuOrders(inputMenus);
+    const dividedMenuOrders = this.getdDvideMenuOrders(inputMenus);
 
-    return this.#menu(dividedMenuOrders.map(menu => { return menu.split('-') }));
+    dividedMenuOrders.forEach(menu => {
+      const [menuName, quantity] = menu.split('-');
+      this.updateOrderedMenu(menuName, +quantity);
+    });
+
+    return this.#orderedMenu;
   }
 
-  getCalculateTotalOrder(dividedMenuAndAmount) {
-    return dividedMenuAndAmount.reduce((orderedPrice, menuAndAmount) => {
-      const menuName = menuAndAmount[NUMBER.menuName];
-      const menuQuantity = +menuAndAmount[NUMBER.menuAmount];
-
-      const menuPrice = Object.keys(MENU).reduce((price, category) => {
-        return menuName in MENU[category] ? MENU[category][menuName] * menuQuantity : price;
-      }, 0);
-
-      return orderedPrice + menuPrice;
+  getTotalOrderedItems() {
+    return Object.keys(this.#orderedMenu).reduce((total, category) => {
+      return total + Object.values(this.#orderedMenu[category]).reduce((subTotal, quantity) => subTotal + quantity, 0);
     }, 0);
   }
 
-  getChristmasDistcount(date) {
-    return
+  updateOrderedMenu(menuName, quantity) {
+    Object.keys(this.#orderedMenu).forEach(category => {
+      if (menuName in MENU[category]) {
+        this.#orderedMenu[category][menuName] += quantity;
+        return;
+      }
+    })
+  }
+
+  getCalculateTotalOrder() {
+    let orderedPrice = 0;
+    Object.keys(this.#orderedMenu).forEach(category => {
+      Object.keys(this.#orderedMenu[category]).forEach(menuName => {
+        orderedPrice += MENU[category][menuName] * this.#orderedMenu[category][menuName];
+      });
+    });
+    return orderedPrice;
   }
 }
 
