3bdc3adf4405da1db3e15b2cdce77613c3f03dcd
kwoncha <rnjsckgus@gmail.com>
2023-11-13 11:31:28 +0900

    feat: 올바르지 않은 형식일 시 throw 기능 추가

--- src/utils/Validation/Validation.js
+++ src/utils/Validation/Validation.js
@@ -1,9 +1,25 @@
 import { DEC_DATE, ORDERED_MENU, MENU } from '../../constants/constants.js';
 import { MESSAGE } from '../../constants/messages.js';
+import MenuCalculation from '../Calculation/MenuCalculation.js';
 
 class Validation {
   isValidDecemberDate(date) {
     if (!this.isValidDateType(date)) throw new Error(MESSAGE.ERROR.notValidDate);
+  }
+
+  isValidMenuOrder(inputMenus) {
+    const dividedMenuArray = MenuCalculation.getdDvideMenuOrders(inputMenus);
+    this.isValidReservationDate(dividedMenuArray);
+  }
+
+  isValidReservationDate(dividedMenuArray) {
+    dividedMenuArray.forEach(menu => {
+      if (!this.isValidMenuType(menu)) throw new Error(MESSAGE.notValidMenuInput);
+    });
+  }
+
+  isValidMenuType(menu) {
+    return ORDERED_MENU.test(menu);
   }
 
   isValidDateType(date) {
