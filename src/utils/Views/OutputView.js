dbd591c079a593c3757c635a25f5e75e1490764a
kwoncha <rnjsckgus@gmail.com>
2023-11-13 11:23:30 +0900

    refactor: 요구사항대로 수정

---
+++ src/utils/Views/OutputView.js
@@ -0,0 +1,15 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  print(text) {
+    MissionUtils.Console.print(text);
+  },
+
+  // printMenu() {
+  //   Console.print('<주문 메뉴>');
+  //   // ...
+  // },
+  // ...
+};
+
+export default OutputView;
