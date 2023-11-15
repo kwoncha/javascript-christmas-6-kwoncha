88caf6b43825309cef8622e2c803575936149f51
kwoncha <rnjsckgus@gmail.com>
2023-11-15 12:36:03 +0900

    test: 총 주문 금액 계산하는 동작 테스트

--- __tests__/MenuCalculationTest.js
+++ __tests__/MenuCalculationTest.js
@@ -34,4 +34,16 @@ describe('이벤트 관련 계산 class 테스트', () => {
       expect(eventBadge).toBe(expected[index]);
     });
   });
+
+  test('총 주문 금액 계산하는 동작 테스트', () => {
+    const expected = [1000, 20000];
+    const amounts = [1000, 22000];
+    const discountAmounts = [0, 2000];
+
+    amounts.forEach((amount, index) => {
+      const totalAmount = menuCalculation.calculateTotalAmount(amount, discountAmounts[index]);
+
+      expect(totalAmount).toBe(expected[index]);
+    });
+  });
 });
